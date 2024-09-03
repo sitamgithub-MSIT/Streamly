b37810b6911e497a00540f471b1c5e7c58cf086d
sitammeur <sitammeur@gmail.com>
2024-09-03 23:16:49 +0000

    game stream added

---
+++ src/source.ts
@@ -0,0 +1,24 @@
+import { game1, game2, game3, game4 } from "./assets";
+
+export const gamesStreams = [
+  {
+    name: "John",
+    image: game1,
+    watching: "1.5k",
+  },
+  {
+    name: "Ross",
+    image: game2,
+    watching: "3.2k",
+  },
+  {
+    name: "Maria",
+    image: game3,
+    watching: "5k",
+  },
+  {
+    name: "Karine",
+    image: game4,
+    watching: "2k",
+  },
+];
