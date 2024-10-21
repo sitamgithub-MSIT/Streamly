6740e9d8218d40328fe2b39155141affd014a5f9
sitammeur <sitammeur@gmail.com>
2024-10-21 19:12:39 +0000

    go live streaming functionality added

--- src/pages/GolivePage.tsx
+++ src/pages/GolivePage.tsx
@@ -1,5 +1,84 @@
-const GolivePage = () => {
-  return <div>GolivePage</div>;
-};
+import { useUser } from "@clerk/clerk-react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import generateUniqueId from "generate-unique-id";
 
-export default GolivePage;
+export function getUrlParams(url = window.location.href) {
+  let urlStr = url.split("?")[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function GolivePage() {
+  const roomID =
+    getUrlParams().get("roomID") || generateUniqueId({ length: 6 });
+  const { user } = useUser();
+  const { fullName, primaryEmailAddress } = user || {};
+  let role_str = getUrlParams(window.location.href).get("role") || "Host";
+  const role =
+    role_str === "Host"
+      ? ZegoUIKitPrebuilt.Host
+      : role_str === "Cohost"
+      ? ZegoUIKitPrebuilt.Cohost
+      : ZegoUIKitPrebuilt.Audience;
+
+  let sharedLinks = [];
+  if (role === ZegoUIKitPrebuilt.Host || role === ZegoUIKitPrebuilt.Cohost) {
+    sharedLinks.push({
+      name: "Join as co-host",
+      url:
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname +
+        "?roomID=" +
+        roomID +
+        "&role=Cohost",
+    });
+  }
+  sharedLinks.push({
+    name: "Join as audience",
+    url:
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?roomID=" +
+      roomID +
+      "&role=Audience",
+  });
+
+  // generate Kit Token
+  const appID = parseInt(import.meta.env.VITE_ZEGO_APP_ID) as number;
+  const serverSecret = import.meta.env.VITE_ZEGO_SERVER_SECRET as string;
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+    appID,
+    serverSecret,
+    roomID,
+    primaryEmailAddress?.id || generateUniqueId({ length: 6 }),
+    fullName || generateUniqueId({ length: 6 })
+  );
+
+  // start the call
+  let myMeeting = async (element: any) => {
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming,
+        config: {
+          role,
+        },
+      },
+      sharedLinks,
+    });
+  };
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: "100vw", height: "100vh" }}
+    ></div>
+  );
+}
